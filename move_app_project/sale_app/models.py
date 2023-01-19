8808cfd093bb42622e1a94ab307aef3752e2017d
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-19 17:33:47 +0600

    added Sale model into sale_ap

--- move_app_project/sale_app/models.py
+++ move_app_project/sale_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+from catalog_app.models import Product
+
 # Create your models here.
+class Sale(models.Model):
+    created = models.DateTimeField()
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
