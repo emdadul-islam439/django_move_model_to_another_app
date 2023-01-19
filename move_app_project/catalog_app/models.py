1776c6c6644f4a03f58d8f715b058b84e4945a4c
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-19 17:31:41 +0600

    added Category and Product model into catalog_app

--- move_app_project/catalog_app/models.py
+++ move_app_project/catalog_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
