5b132492ee4a9656db108503a1246690b2ba4935
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-26 03:42:56 +0600

    registered product_app/Product model into the admin.py file and generated migrations for copying category_app/Product model's data into product_app/Product model

--- move_app_project/catalog_app/models.py
+++ move_app_project/catalog_app/models.py
@@ -6,4 +6,4 @@ class Category(models.Model):
     
 class Product(models.Model):
     name = models.CharField(max_length=100, db_index=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='+')
