67a219ac6072c1d790970386b1edf4b07365bab1
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-27 01:10:32 +0600

    Last Step: deleted the db_index=True parameter from product_app/Product model and created migration file using 'makemigrations' command. checked the underlying sql command by 'sqlmigrate product_app 0002' command. all were fine! Lastly applied the migrations

--- move_app_project/product_app/models.py
+++ move_app_project/product_app/models.py
@@ -4,7 +4,7 @@ from catalog_app.models import Category
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     
     class Meta:
