69fc74f54928d8e5280a842eb14033968fd62725
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-27 02:44:32 +0600

    created migration files by modifying 'db_index=True/False' property of the product_app/Product model to see how sqlite3 handle the renaming

--- move_app_project/product_app/models.py
+++ move_app_project/product_app/models.py
@@ -4,5 +4,5 @@ from catalog_app.models import Category
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
