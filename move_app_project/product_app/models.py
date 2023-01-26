bfcb4f877cb9aad87b7079e17e40a70e48cbede7
Emdadul_Islam <emdadulislam439@gmail.com>
2023-01-26 23:38:32 +0600

    added Meta class with db_table='catalog_app_product' property

--- move_app_project/product_app/models.py
+++ move_app_project/product_app/models.py
@@ -6,3 +6,6 @@ from catalog_app.models import Category
 class Product(models.Model):
     name = models.CharField(max_length=100, db_index=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'catalog_app_product'
